da98bd7aea020e0684d0772bc23f100955f169e2
EoJin <141602161+KimEoJin24@users.noreply.github.com>
2023-10-16 09:48:43 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.64 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(string t, string p) {
+        int answer = 0;
+        
+        for(int i=0; i<t.Length-p.Length+1; i++){
+            long num = long.Parse(t.Substring(i,p.Length));
+            if(num <= long.Parse(p)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
