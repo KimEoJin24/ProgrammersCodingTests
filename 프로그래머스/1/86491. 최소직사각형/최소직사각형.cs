07fbdc40b6bd7d94436cb0d24a696c56a8466d7a
EoJin <141602161+KimEoJin24@users.noreply.github.com>
2023-10-17 07:43:17 +0900

    [level 1] Title: 최소직사각형, Time: 0.36 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int solution(int[,] sizes) {
+        int answer = 0;
+        int maxW = 0;
+        int maxH = 0;
+        
+        for(int i=0; i<sizes.GetLength(0); i++){
+            int width = sizes[i,0];
+            int height = sizes[i,1];
+            if(height > width){
+                width = sizes[i,1];
+                height = sizes[i,0];
+            }
+            
+            if(width > maxW){
+                maxW = width;
+            }
+            if(height > maxH){
+                maxH = height;
+            }
+        }
+        answer = maxW * maxH;
+        return answer;
+    }
+}
